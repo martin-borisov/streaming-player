fb257a98659238af4e2851a23bd1488e54156e56
Martin <mb.borisov@gmail.com>
2024-07-15 17:40:55 +0300

    Fixed source label truncation issue

--- src/main/java/mb/player/components/swing/PlaylistCellRenderer.java
+++ src/main/java/mb/player/components/swing/PlaylistCellRenderer.java
@@ -1,11 +1,14 @@
 package mb.player.components.swing;
 
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Font;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
 
 import org.apache.commons.lang3.StringUtils;
@@ -13,15 +16,17 @@ import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
 import org.kordamp.ikonli.swing.FontIcon;
 
 import mb.player.media.MPMedia;
+import net.miginfocom.swing.MigLayout;
 
-public class PlaylistCellRenderer extends JLabel implements ListCellRenderer<MPMedia> {
+public class PlaylistCellRenderer extends JPanel implements ListCellRenderer<MPMedia> {
     private static final long serialVersionUID = 1L;
     
+    private JLabel nameLabel, sourceLabel;
     private MPMedia currentlyPlayingMedia;
     private boolean showSource;
     
     public PlaylistCellRenderer() {
-        setOpaque(true);
+        createAndLayoutComponents();
     }
     
     public PlaylistCellRenderer(boolean showSource) {
@@ -32,21 +37,16 @@ public class PlaylistCellRenderer extends JLabel implements ListCellRenderer<MPM
     @Override
     public Component getListCellRendererComponent(JList<? extends MPMedia> list, MPMedia media, int index,
             boolean isSelected, boolean cellHasFocus) {
-        
-        StringBuilder buf = new StringBuilder();
-        buf.append("<html><body>");
-        buf.append("<p><b>").append(media.getName()).append("</b></p>");
+        nameLabel.setIcon(media == currentlyPlayingMedia ? FontIcon.of(FontAwesomeSolid.PLAY, 15) : null);
+        nameLabel.setText(media.getName());
         
         if(showSource && StringUtils.isNotBlank(media.getSource())) {
             String source = URLDecoder.decode(media.getSource(), StandardCharsets.UTF_8);
             source = StringUtils.removeStart(source, "file:");
-            source = StringUtils.abbreviate(source, 100);
-            buf.append("<p><i style=\"color: #778899;\">").append(source).append("</i></p>");
+            sourceLabel.setText(source);
+        } else {
+            sourceLabel.setText(StringUtils.EMPTY);
         }
-        buf.append("</body></html>");
-        
-        setIcon(media == currentlyPlayingMedia ? FontIcon.of(FontAwesomeSolid.PLAY, 15) : null);
-        setText(buf.toString());
         
         if (isSelected) {
             setBackground(list.getSelectionBackground());
@@ -68,5 +68,17 @@ public class PlaylistCellRenderer extends JLabel implements ListCellRenderer<MPM
 
     public void setShowSource(boolean showSource) {
         this.showSource = showSource;
+    }
+    
+    private void createAndLayoutComponents() {
+        setLayout(new MigLayout("insets 0, gap 0, fill, wrap", "[]", "[][]"));
+        
+        nameLabel = new JLabel();
+        nameLabel.setFont(nameLabel.getFont().deriveFont(nameLabel.getFont().getStyle() | Font.BOLD));
+        add(nameLabel, "width 0::");
+        sourceLabel = new JLabel();
+        sourceLabel.setFont(sourceLabel.getFont().deriveFont(sourceLabel.getFont().getStyle() | Font.ITALIC));
+        sourceLabel.setForeground(new Color(Integer.parseInt("778899", 16)));
+        add(sourceLabel, "width 0::");
     }
 }
