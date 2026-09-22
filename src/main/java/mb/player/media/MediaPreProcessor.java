48fee2a3f5ce0de49dcaaa5803b17e219bedc0b8
Martin <mb.borisov@gmail.com>
2026-09-22 17:14:01 +0300

    Adding support for attributes of remote flac files

--- src/main/java/mb/player/media/MediaPreProcessor.java
+++ src/main/java/mb/player/media/MediaPreProcessor.java
@@ -5,7 +5,9 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.net.HttpURLConnection;
 import java.net.URI;
+import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -63,66 +65,90 @@ public class MediaPreProcessor {
         }
         
         // Fetch attributes
+        if(media.getSource().endsWith("flac")) {
+            processFlac(media);
+        } else if(media.getSource().endsWith("mp3")) {
+            processMp3(media);
+        } else if(media.getSource().endsWith("wav")) {
+            // TODO Support for WAV metadata
+        }
+    }
+    
+    private void processMp3(MPMedia media) {
         if(media.isLocal()) {
             File audioFile = new File(URI.create(media.getSource()));
+            try {
+                Mp3File mp3file = new Mp3File(audioFile);
+                durationSec = mp3file.getLengthInSeconds();
+                sampleRate = mp3file.getSampleRate();
+                totalSamples = sampleRate * durationSec;
+
+                if (mp3file.hasId3v2Tag()) {
+                    ID3v2 id3v2Tag = mp3file.getId3v2Tag();
+                    attributes.put("title", id3v2Tag.getTitle());
+                    attributes.put("artist", id3v2Tag.getArtist());
+                    attributes.put("album", id3v2Tag.getAlbum());
+
+                    // TODO Fetch more mp3 attributes
+
+                    // Fetch album art if not already available as cover file
+                    if (attributes.get("artwork") == null) {
+                        byte[] bytes = id3v2Tag.getAlbumImage();
+                        if (bytes != null) {
+                            attributes.put("artwork", MPUtils.imageFromID3TagSwing(
+                                    new ByteArrayInputStream(bytes)));
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LOG.log(Level.WARNING, "MP3 pre processing failed", e);
+            }
+        } else {
+            // TODO Support for remote mp3 files
+        }
+    }
+    
+    private void processFlac(MPMedia media) {
+        FlacAudioFileReader reader = new FlacAudioFileReader();
+        try {
             
-            if(media.getSource().endsWith("flac")) {
-
-                FlacAudioFileReader reader = new FlacAudioFileReader();
-                try {
-					AudioFileFormat format = reader.getAudioFileFormat(audioFile);
-					
-					// Getting the total sample count requires a quick hack unfortunately
-					Field streamInfoField = reader.getClass().getDeclaredField("streamInfo");
-					streamInfoField.setAccessible(true);
-					StreamInfo info = (StreamInfo) streamInfoField.get(reader);
-                    if(info != null) {
-                        totalSamples = info.getTotalSamples();
-                        sampleRate = info.getSampleRate();
-                        
-                        if(totalSamples > 0 && sampleRate > 0) {
-                            durationSec = (long) (totalSamples / sampleRate);
-                        }
-                    }
-					
-					// Fetch properties
-                    attributes.putAll(format.properties());
-
-				} catch (Exception e) {
-					LOG.log(Level.WARNING, "FLAC pre processing failed", e);
-				}
-            } else if(media.getSource().endsWith("mp3")) {
+            AudioFileFormat format;
+            if(media.isLocal()) {
                 
-                try {
-                    Mp3File mp3file = new Mp3File(audioFile);
-                    durationSec = mp3file.getLengthInSeconds();
-                    sampleRate = mp3file.getSampleRate();
-                    totalSamples = sampleRate * durationSec;
-                    
-                    if(mp3file.hasId3v2Tag()) {
-                        ID3v2 id3v2Tag = mp3file.getId3v2Tag();
-                        attributes.put("title", id3v2Tag.getTitle());
-                        attributes.put("artist", id3v2Tag.getArtist());
-                        attributes.put("album", id3v2Tag.getAlbum());
-                        
-                        // TODO Fetch more attributes
-                        
-                        // Fetch album art
-                        if(attributes.get("artwork") == null) {
-                            byte[] bytes = id3v2Tag.getAlbumImage();
-                            if(bytes != null) {
-                                attributes.put("artwork", 
-                                        MPUtils.imageFromID3TagSwing(new ByteArrayInputStream(bytes)));
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    LOG.log(Level.WARNING, "MP3 pre processing failed", e);
+                File audioFile = new File(URI.create(media.getSource()));
+                format = reader.getAudioFileFormat(audioFile);
+            } else {
+                
+                URL url = URI.create(media.getSource()).toURL();
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setAuthenticator(MPUtils.createAuthenticator(media));
+                if(con.getResponseCode() != 200) {
+                    LOG.log(Level.WARNING, "FLAC pre processing failed; received HTTP response code: {0}", 
+                            new Object[]{con.getResponseCode()});
+                    return;
                 }
                 
-            } else if(media.getSource().endsWith("wav")) {
-                // TODO
+                format = reader.getAudioFileFormat(con.getInputStream());
             }
+
+            // Getting the total sample count requires a quick hack unfortunately
+            Field streamInfoField = reader.getClass().getDeclaredField("streamInfo");
+            streamInfoField.setAccessible(true);
+            StreamInfo info = (StreamInfo) streamInfoField.get(reader);
+            if (info != null) {
+                totalSamples = info.getTotalSamples();
+                sampleRate = info.getSampleRate();
+
+                if (totalSamples > 0 && sampleRate > 0) {
+                    durationSec = (long) (totalSamples / sampleRate);
+                }
+            }
+
+            // Fetch properties
+            attributes.putAll(format.properties());
+
+        } catch (Exception e) {
+            LOG.log(Level.WARNING, "FLAC pre processing failed", e);
         }
     }
 
